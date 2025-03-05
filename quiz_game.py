a905dc0420227c8244269dd3f73ee1577948bf03
Lukzee <ibrahimlukmanbolakale2016@gmail.com>
2025-03-05 19:52:24 +0100

    new commit

--- quiz_game.py
+++ quiz_game.py
@@ -5,41 +5,49 @@ QUESTION = 'question'
 OPTIONS = 'options'
 ANSWER = 'answer'
 
-# question = input()
-quiz = [
-    {
-      QUESTION: 'What is the capital of France?',
-      OPTIONS: ['A. Berlin', 'B. Madrid', 'C. Paris', 'D. Rome'],
-      ANSWER: 'C'
-    },
-    {
-      QUESTION: 'Which planet is known as the red planet?',
-      OPTIONS: ['A. Earth', 'B. Mars', 'C. Jupiter', 'D. Saturn'],
-      ANSWER: 'B'
-    },
-    {
-      QUESTION: 'What is the largest ocean on Earth?',
-      OPTIONS: ['A. Atlantic', 'B. Indian', 'C. Arctic', 'D. Pacific'],
-      ANSWER: 'D'
-    }
-]
+def ask_question(index, question, options):
+    print(f"Question {index}: {question}")
 
-score = 0
-random.shuffle(quiz)
-
-for index, item in enumerate(quiz, 1):
-    print(f"Question {index}: {item[QUESTION]}")
-
-    for option in item[OPTIONS]:
+    for option in options:
         print(option)
-    answer = input("Your answer ").upper().strip()
+    return input("Your answer ").upper().strip()
 
-    if answer == item[ANSWER]:
-        cprint('Correct!', 'green')
-        score +=1
-    else:
-        cprint(f'Wrong!, the correct answer is {item[ANSWER]}', 'red')
+def start_quiz(quiz):
+    random.shuffle(quiz)
+    score = 0
 
-    print()
+    for index, item in enumerate(quiz, 1):
+        answer = ask_question(index, item[QUESTION], item[OPTIONS])
 
-print(f"Quiz over!, your final score is {score} out of {len(quiz)}")
+        if answer == item[ANSWER]:
+            cprint('Correct!', 'green')
+            score += 1
+        else:
+            cprint(f'Wrong!, the correct answer is {item[ANSWER]}', 'red')
+
+        print()
+
+    print(f"Quiz over!, your final score is {score} out of {len(quiz)}")
+
+def main():
+    quiz = [
+        {
+            QUESTION: 'What is the capital of France?',
+            OPTIONS: ['A. Berlin', 'B. Madrid', 'C. Paris', 'D. Rome'],
+            ANSWER: 'C'
+        },
+        {
+            QUESTION: 'Which planet is known as the red planet?',
+            OPTIONS: ['A. Earth', 'B. Mars', 'C. Jupiter', 'D. Saturn'],
+            ANSWER: 'B'
+        },
+        {
+            QUESTION: 'What is the largest ocean on Earth?',
+            OPTIONS: ['A. Atlantic', 'B. Indian', 'C. Arctic', 'D. Pacific'],
+            ANSWER: 'D'
+        }
+    ]
+    start_quiz(quiz)
+
+if __name__ == '__main__':
+    main()
